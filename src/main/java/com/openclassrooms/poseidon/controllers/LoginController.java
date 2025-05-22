e57258f85be62dd1f0fadf1fc2191771b8b444fc
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-22 18:04:32 +0200

    domain classes mapping

--- src/main/java/com/openclassrooms/poseidon/controllers/LoginController.java
+++ src/main/java/com/openclassrooms/poseidon/controllers/LoginController.java
@@ -1,6 +1,6 @@
 package com.openclassrooms.poseidon.controllers;
 
-import com.nnk.springboot.repositories.UserRepository;
+import com.openclassrooms.poseidon.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/openclassrooms/poseidon/domain/RuleName.java
+++ src/main/java/com/openclassrooms/poseidon/domain/RuleName.java
@@ -1,10 +1,21 @@
 package com.openclassrooms.poseidon.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 @Table(name = "rulename")
 public class RuleName {
-    // TODO: Map columns in data table RULENAME with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    Integer id;
+
+    String name;
+    String description;
+    String json;
+    String template;
+    String sqlStr;
+    String sqlPart;
 }
