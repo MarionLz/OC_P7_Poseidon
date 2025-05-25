6960709108bb129b1eb424f4fe0a612255174c5b
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-25 12:05:10 +0200

    bug fixes in UserController

--- src/main/java/com/openclassrooms/poseidon/controllers/UserController.java
+++ src/main/java/com/openclassrooms/poseidon/controllers/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
@@ -20,19 +17,19 @@ public class UserController {
     private UserRepository userRepository;
 
     @RequestMapping("/user/list")
-    public String home(Model model)
-    {
+    public String home(Model model) {
         model.addAttribute("users", userRepository.findAll());
         return "user/list";
     }
 
     @GetMapping("/user/add")
-    public String addUser(UserEntity bid) {
+    public String addUser(Model model) {
+        model.addAttribute("user", new UserEntity());
         return "user/add";
     }
 
     @PostMapping("/user/validate")
-    public String validate(@Valid UserEntity user, BindingResult result, Model model) {
+    public String validate(@Valid @ModelAttribute("user") UserEntity user, BindingResult result, Model model) {
         if (!result.hasErrors()) {
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
             user.setPassword(encoder.encode(user.getPassword()));
