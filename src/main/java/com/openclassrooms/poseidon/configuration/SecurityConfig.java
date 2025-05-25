8ac74443881df31b4141375fc91ca605da26cdff
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-25 11:44:48 +0200

    LoginService added

--- src/main/java/com/openclassrooms/poseidon/configuration/SecurityConfig.java
+++ src/main/java/com/openclassrooms/poseidon/configuration/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/", "/app/login").permitAll()
+                        .requestMatchers("/css/**", "/", "/app/login", "/user/list", "/user/add", "/user/validate").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
