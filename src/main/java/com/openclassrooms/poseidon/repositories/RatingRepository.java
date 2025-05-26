f1c6e0ec73cbb34aec91af86a11083326fe46034
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-26 15:44:00 +0200

    RatingController implemented

--- src/main/java/com/openclassrooms/poseidon/repositories/RatingRepository.java
+++ src/main/java/com/openclassrooms/poseidon/repositories/RatingRepository.java
@@ -1,8 +1,8 @@
 package com.openclassrooms.poseidon.repositories;
 
-import com.openclassrooms.poseidon.domain.Rating;
+import com.openclassrooms.poseidon.domain.RatingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RatingRepository extends JpaRepository<Rating, Integer> {
+public interface RatingRepository extends JpaRepository<RatingEntity, Integer> {
 
 }
