81e603833043527cc516dad83b5e04d8c2426821
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-28 10:31:17 +0200

    BidListController implemented

--- src/main/java/com/openclassrooms/poseidon/controllers/BidListController.java
+++ src/main/java/com/openclassrooms/poseidon/controllers/BidListController.java
@@ -1,53 +1,92 @@
 package com.openclassrooms.poseidon.controllers;
 
-import com.openclassrooms.poseidon.domain.BidList;
+import com.openclassrooms.poseidon.domain.BidListEntity;
+import com.openclassrooms.poseidon.domain.RuleNameEntity;
+import com.openclassrooms.poseidon.repositories.BidListRepository;
+import com.openclassrooms.poseidon.repositories.RuleNameRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class BidListController {
-    // TODO: Inject Bid service
+
+    private static final Logger logger = LogManager.getLogger("BidListController");
+
+    @Autowired
+    private BidListRepository bidListRepository;
 
     @RequestMapping("/bidList/list")
-    public String home(Model model)
+    public String home(Model model, HttpServletRequest request)
     {
-        // TODO: call service find all bids to show to the view
+        model.addAttribute("bidLists", bidListRepository.findAll());
+        model.addAttribute("httpServletRequest", request);
+        logger.info("GET /bidList/list - OK");
         return "bidList/list";
     }
 
     @GetMapping("/bidList/add")
-    public String addBidForm(BidList bid) {
+    public String addBidForm(Model model) {
+
+        model.addAttribute("bidList", new BidListEntity());
+        logger.info("GET /bidList/add - OK");
         return "bidList/add";
     }
 
     @PostMapping("/bidList/validate")
-    public String validate(@Valid BidList bid, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return bid list
+    public String validate(@Valid @ModelAttribute("bidList") BidListEntity bidList, BindingResult result, Model model,
+                           RedirectAttributes ra) {
+
+        if (!result.hasErrors()) {
+            bidListRepository.save(bidList);
+            model.addAttribute("bidLists", bidListRepository.findAll());
+            ra.addFlashAttribute("successMessage", "Bid list created successfully");
+            logger.info("POST /bidList/validate - OK");
+            return "redirect:/bidList/list";
+        }
+        logger.warn("POST /bidList/validate - KO : validation errors found");
         return "bidList/add";
     }
 
     @GetMapping("/bidList/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get Bid by Id and to model then show to the form
+
+        BidListEntity bidList = bidListRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Bid list Id:" + id));
+        model.addAttribute("bidList", bidList);
+        logger.info("GET /bidList/update/{} - OK", id);
         return "bidList/update";
     }
 
     @PostMapping("/bidList/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Bid and return list Bid
+    public String updateBid(@PathVariable("id") Integer id, @Valid BidListEntity bidList,
+                             BindingResult result, Model model, RedirectAttributes ra) {
+
+        if (result.hasErrors()) {
+            logger.warn("POST /bidList/update/{} - KO : validation errors found", id);
+            return "bidList/update";
+        }
+        bidListRepository.save(bidList);
+        model.addAttribute("bidLists", bidListRepository.findAll());
+        ra.addFlashAttribute("successMessage", "Bid list updated successfully");
+        logger.info("POST /bidList/update/{} - OK", id);
         return "redirect:/bidList/list";
     }
 
     @GetMapping("/bidList/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Bid by Id and delete the bid, return to Bid list
+    public String deleteBid(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+
+        BidListEntity bidList = bidListRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid Bid list Id:" + id));
+        bidListRepository.delete(bidList);
+        model.addAttribute("bidLists", bidListRepository.findAll());
+        ra.addFlashAttribute("successMessage", "Bid list deleted successfully");
+        logger.info("GET /bidList/delete/{} - OK", id);
         return "redirect:/bidList/list";
     }
 }
