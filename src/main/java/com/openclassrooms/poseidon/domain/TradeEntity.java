0de03e1ff4aa952b21aaf229b5a3583f389e1fbc
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-05-31 18:28:07 +0200

    TradeService implemented

--- src/main/java/com/openclassrooms/poseidon/domain/TradeEntity.java
+++ src/main/java/com/openclassrooms/poseidon/domain/TradeEntity.java
@@ -2,6 +2,7 @@ package com.openclassrooms.poseidon.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -25,6 +26,7 @@ public class TradeEntity {
     @Size(max = 30, message = "Type must be less than 30 characters")
     String type;
 
+    @NotNull(message = "Buy quantity is mandatory")
     @PositiveOrZero(message = "Buy quantity must be zero or positive")
     Double buyQuantity;
 
